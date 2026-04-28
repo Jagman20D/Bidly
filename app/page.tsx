00023dfa4196d76f4b1f6685992e8380f2a72ee0
Jagman20D <the_nice_one@live.ca>
2026-04-28 17:48:39 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 40 }}>
       <h1>Bidly is live!</h1>
-      <p>Your deployment succeeded. Now you can build your homepage.</p>
+      <p>Your deployment succeeded. This is your homepage.</p>
     </main>
   );
 }
