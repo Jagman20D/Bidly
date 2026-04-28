eaa1684b360b756d88bb00d561d9dfe5ccbb32fd
Jagman20D <the_nice_one@live.ca>
2026-04-28 15:11:00 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,15 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ padding: 40 }}>
       <h1>Welcome to Bidly</h1>
 
       <Link href="/create-listing">
-        <button style={{ padding: "10px 20px", marginTop: "20px" }}>
+        <button style={{ padding: "10px 20px", marginTop: 20 }}>
           Create a Listing
         </button>
       </Link>
     </main>
   );
 }
+
 
