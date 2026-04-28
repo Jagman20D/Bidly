bc92ee4382933c2ad2a0d988eb6f483da7cd1ee0
Jagman20D <the_nice_one@live.ca>
2026-04-28 16:36:57 -0400

    Update supabaseClient.ts

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+// supabase client (patched to force new commit)
+
+import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
